8c1b3b75fb81326bf1af43e58ceb7e9c2c76633c
sottomarino <simone.ambrosi@live.com>
2021-11-17 18:04:10 +0100

    Updates

---
+++ precache-manifest.6cd6a1b384b7134a5e184bebd3b0b9d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "256c7a8dcda15c67de89",
+    "url": "/game-master2/static/css/main.d66aa19c.chunk.css"
+  },
+  {
+    "revision": "256c7a8dcda15c67de89",
+    "url": "/game-master2/static/js/main.256c7a8d.chunk.js"
+  },
+  {
+    "revision": "2d7a9d8f6f6084b588bb",
+    "url": "/game-master2/static/css/1.97326e9d.chunk.css"
+  },
+  {
+    "revision": "2d7a9d8f6f6084b588bb",
+    "url": "/game-master2/static/js/1.2d7a9d8f.chunk.js"
+  },
+  {
+    "revision": "911c2dafd71904f615b5",
+    "url": "/game-master2/static/js/runtime~main.911c2daf.js"
+  },
+  {
+    "revision": "69f251f74a23ed77c67bcd0066f94b4c",
+    "url": "/game-master2/index.html"
+  }
+];
